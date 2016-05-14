d5227fe6e5b28799648af5a4117027221f217723
boxcore <boxcore@live.com>
2016-05-14 17:57:19 +0800

    fix bug

--- da/block_ips/install_block_ip.sh
+++ da/block_ips/install_block_ip.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 mv /etc/init.d/iptables /etc/init.d/iptables.backup
-mv iptables /etc/init.d/iptables
-chmod 755 iptables
+cp iptables /etc/init.d/iptables
+chmod 755 /etc/init.d/iptables
 /etc/init.d/iptables restart
-mv block_ip.sh /usr/local/directadmin/scripts/custom/block_ip.sh
-mv show_blocked_ips.sh /usr/local/directadmin/scripts/custom/show_blocked_ips.sh
-mv unblock_ip.sh /usr/local/directadmin/scripts/custom/unblock_ip.sh
-mv brute_force_notice_ip.sh /usr/local/directadmin/scripts/custom/brute_force_notice_ip.sh
+cp block_ip.sh /usr/local/directadmin/scripts/custom/block_ip.sh
+cp show_blocked_ips.sh /usr/local/directadmin/scripts/custom/show_blocked_ips.sh
+cp unblock_ip.sh /usr/local/directadmin/scripts/custom/unblock_ip.sh
+cp brute_force_notice_ip.sh /usr/local/directadmin/scripts/custom/brute_force_notice_ip.sh
 chmod 700 /usr/local/directadmin/scripts/custom/*.sh
 touch /root/blocked_ips.txt
 touch /root/exempt_ips.txt
