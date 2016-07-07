484510bf685b78660298fdc077bbe45553e8c1d3
boxcore <boxcore@live.com>
2016-07-07 13:21:29 +0800

    add new script

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum install gettext curl curl-devel zlib-devel openssl-devel perl cpio expat-devel gettext-devel -y
-
+# https://www.kernel.org/pub/software/scm/git/git-2.9.0.tar.gz
 wget https://git-core.googlecode.com/files/git-1.8.3.2.tar.gz
 tar xzvf git-1.8.3.2.tar.gz
 cd git-1.8.3.2
---
+++ install-3rd/lnmp1.3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~
+wget -c http://soft.vpser.net/lnmp/lnmp1.3-full.tar.gz && tar zxf lnmp1.3-full.tar.gz && cd lnmp1.3-full && ./install.sh lnmp
