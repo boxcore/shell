51d215faa976ab854ef5c77ee4aed9837b388da8
boxcore <boxcore@live.com>
2016-07-13 14:20:16 +0800

    add python3 script

---
+++ tools/install_python3.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+cd ~
+wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+tar -zxf Python-3.5.2.tgz 
+cd Python-3.5.2
+./configure --prefix=/usr/local/python3
+make && make install
+
+cd ~
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+
+
