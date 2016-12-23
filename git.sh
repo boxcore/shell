330e140b4e1316e53bc266727e642c6b4eae6e20
boxcore <boxcore@live.com>
2016-12-23 10:16:08 +0800

    update git version to v2.11

--- git.sh
+++ git.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 yum install gettext curl curl-devel zlib-devel openssl-devel perl cpio expat-devel gettext-devel -y
+yum -y install zlib-devel curl-devel openssl-devel perl cpio expat-devel gettext-devel openssl zlib autoconf tk perl-ExtUtils-MakeMaker
+
 # https://www.kernel.org/pub/software/scm/git/git-2.9.0.tar.gz
-wget https://git-core.googlecode.com/files/git-1.8.3.2.tar.gz
-tar xzvf git-1.8.3.2.tar.gz
-cd git-1.8.3.2
+wget -o 'git-2.11.0.tar.gz' https://github.com/git/git/archive/v2.11.0.tar.gz
+tar xzvf git-2.11.0.tar.gz
+cd git-2.11.0
 
 autoconf
 ./configure
 make
 sudo make install
+
+#export PATH=$PATH:/usr/local/bin
 
 git --version
