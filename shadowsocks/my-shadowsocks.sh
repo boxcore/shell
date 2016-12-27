0277105b015960683226576bdb1900de9537b5ce
boxcore <boxcore@live.com>
2016-12-27 16:17:06 +0800

    add action rule for sss

--- shadowsocks/my-shadowsocks.sh
+++ shadowsocks/my-shadowsocks.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 chmod +x shadowsocks-go.sh
-./my-shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+/bin/bash ./my-shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
 service shadowsocks restart
 chkconfig shadowsocks on
