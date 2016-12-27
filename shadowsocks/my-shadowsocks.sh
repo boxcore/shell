23e14b7d63975f73a146373b33b4cadec9af713a
boxcore <boxcore@live.com>
2016-12-27 16:15:06 +0800

    setting default my sss

--- shadowsocks/my-shadowsocks.sh
+++ shadowsocks/my-shadowsocks.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 chmod +x shadowsocks-go.sh
-./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+./my-shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
 service shadowsocks restart
 chkconfig shadowsocks on
