36e5b81c08cd407caa6995d39db904b355869958
boxcore <boxcore@live.com>
2017-03-22 10:40:49 +0800

    add hk-mount-check.sh and commit

--- tools/hk-mount-check.sh
+++ tools/hk-mount-check.sh
@@ -11,6 +11,13 @@ else
         lvchange -a y /dev/mapper/Xvdbgroup-xvdb1
         mount /dev/mapper/Xvdbgroup-xvdb1 /home
         resize2fs -p /dev/mapper/Xvdbgroup-xvdb1
+    /etc/rc.d/init.d/nginx restart
+/etc/rc.d/init.d/mysql restart
+/etc/rc.d/init.d/php-fpm restart
+/etc/rc.d/init.d/php-fpm52 restart
+/etc/rc.d/init.d/pureftpd restart
+/etc/rc.d/init.d/shadowsocks restart
+/etc/rc.d/init.d/crond restart
     else
         echo 'already mount!'
     fi
