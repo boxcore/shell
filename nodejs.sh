0ec00bf5880909bf22465bc750ef6702e21021dc
Box Core <boxcore@live.com>
2017-08-31 17:56:31 +0800

    add node.js new version 6.11

--- nodejs.sh
+++ nodejs.sh
@@ -5,7 +5,7 @@ export PATH
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script, please use root to install lnmp"
+    echo "Error: You must be root to run this script, please use root to install node.js"
     exit 1
 fi
 hr="========================================================================="
@@ -17,9 +17,10 @@ cur_dir=`pwd`
 function InstallNodejs()
 {
     cd $cur_dir
-    wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
-    tar zxf node-v0.10.26.tar.gz
-    cd node-v0.10.26
+    # wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
+    wget https://nodejs.org/dist/v6.11.2/node-v6.11.2.tar.gz
+    tar zxf node-v6.11.2.tar.gz
+    cd node-v6.11.2
     ./configure
     make && make install
 }
